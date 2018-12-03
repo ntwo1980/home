e17c0d0598225ed99ae55c77563a806b1154cd3f
ntwo1980 <ntwo1980@gmail.com>
2018-12-03 14:20:27 +0800

    update

---
+++ Documents/scripts/init.sh
@@ -0,0 +1,11 @@
+apt-get install update
+apt-get install upgrade
+
+apt-get install git
+apt-get install zsh
+
+
+git clone git@github.com:ntwo1980/home.git ~
+git clone git@github.com:zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone git@github.com:zsh-users/zsh-completions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
